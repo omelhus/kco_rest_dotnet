feb88e79428f19e3083d7d1e52c6e173732d6ce1
Ole Melhus <ole@on-it.no>
2019-12-11 16:04:19 +0100

    Fix namespace

--- Klarna.Rest/Klarna.Rest.Core/Model/SettlementsGetTransactionsResponse.cs
+++ Klarna.Rest/Klarna.Rest.Core/Model/SettlementsGetTransactionsResponse.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using Newtonsoft.Json;
 
-namespace Klarna.Rest.Core.Model
+namespace Klarna.Rest.Core.Model.Settlements
 {
     /// <summary>
     /// Get transaction response object
